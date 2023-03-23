1203bca9b976a7cabb9dbddb4eb2953725486536
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-03-23 18:45:28 +0100

    Improve solution day-8

--- dia-8/solution.js
+++ dia-8/solution.js
@@ -3,10 +3,10 @@
 function createCalculator() {
   let total = 0;
 
-  const add = (value) => (total = total + value);
-  const subtract = (value) => (total = total - value);
-  const multiply = (value) => (total = total * value);
-  const divide = (value) => (total = total / value);
+  const add = (value) => (total += value);
+  const subtract = (value) => (total -= value);
+  const multiply = (value) => (total *= value);
+  const divide = (value) => (total /= value);
   const clear = () => (total = 0);
   const getTotal = () => total;
 
